290b369767159dc8934dd433ed6d6afe8e9acd33
Jacob84 <jacob.mendoza@gmail.com>
2018-04-22 21:00:25 +0200

    Initial full stack card

--- src/cards/atom_packages_evolution/intro.cljs
+++ src/cards/atom_packages_evolution/intro.cljs
@@ -1,5 +1,7 @@
 (ns atom-packages-evolution.intro
   (:require [devcards.core :as rc :refer-macros [defcard]]
+            [fulcro.client.cards :refer [defcard-fulcro]]
+            [fulcro.client.data-fetch :as df]
             [atom-packages-evolution.ui.components :as comp]))
 
 (defcard AtomPackage
@@ -12,3 +14,11 @@
    {:packages-list/items [{:db/id 1 :package/name "a" :package/downloads 1 :package/stargazers 2}
                           {:db/id 2 :package/name "b" :package/downloads 3 :package/stargazers 4}
                           {:db/id 3 :package/name "c" :package/downloads 5 :package/stargazers 6}]}))
+
+(defcard-fulcro Application
+  comp/AtomPackagesList
+  {}
+  {:inspect-data true
+   :fulcro {:started-callback (fn [app]
+                                (df/load app :packages-list/items comp/AtomPackage)
+                                (js/console.log :STARTED))}})
--- src/main/atom_packages_evolution/api/read.clj
+++ src/main/atom_packages_evolution/api/read.clj
@@ -4,5 +4,7 @@
     [fulcro.i18n :as i18n]
     [taoensso.timbre :as timbre]))
 
-;; Server queries can go here
-
+(defquery-root :packages-list/items
+  (value [env params]
+    [{:db/id 1 :package/name "Package 1" :package/downloads 999 :package/stargazers 777}
+     {:db/id 2 :package/name "Package 2" :package/downloads 888 :package/stargazers 555}]))
