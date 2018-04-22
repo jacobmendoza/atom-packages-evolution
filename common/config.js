b9110aac2f6145324e576e504fd400a5b0d07161
Jacob84 <jacob.mendoza@gmail.com>
2018-04-22 13:21:55 +0200

    JS code removed

--- common/config.js
+++
@@ -1,9 +0,0 @@
-  const config  = {
-    db: {
-      production: 'mongodb://127.0.0.1/atom-packages-evolution',
-      test: 'mongodb://127.0.0.1/atom-packages-evolution-test'
-    },
-    timeBetweenCrawl: 5000
-  };
-
-  module.exports = config;
